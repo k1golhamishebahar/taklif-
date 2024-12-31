6091c9370276fba6ea33e8e7791fcaa76e550685
k1 <167433832+k1golhamishebahar@users.noreply.github.com>
2024-12-31 20:21:59 +0330

    c#
    t

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int usernum;
+            string number;
+            number = Console.ReadLine();
+            usernum = int.Parse(number);
+            Console.WriteLine(f(usernum));
+            Console.ReadLine();
+        }
+    static int f(int x)
+        {
+            return x * x;           
+        } 
+    static int fib(int i)
+        {
+            if (i == 1)
+                return 0;
+            else if (i == 2)
+                return 1;
+            else 
+                return fib(i - 1) + fib(i - 2);
+        }        
+    }
+}
